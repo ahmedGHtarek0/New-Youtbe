f37cad11b829d7fa21d4e3f5489e2cd39e02d2d8
ahmed tarek <djdndienowkwon@gmail.com>
2025-03-13 21:58:51 +0200

    test

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+const port = 3001
+app.use(express.json())
+mongoose.connect("mongodb://localhost:27017/youtubeNew").then(() => console.log('ahmed test')).catch((i)=>console.log('errr' ,i))
+
+app.listen(port,()=>{
+    console.log('server is running')
+}
+)
