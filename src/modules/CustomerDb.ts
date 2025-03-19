19df9ea8dbde709ee2ce58fd134aac2a0c0c94b3
ahmed tarek <djdndienowkwon@gmail.com>
2025-03-19 21:37:25 +0200

    update in modules/CustomerDb

--- src/modules/CustomerDb.ts
+++ src/modules/CustomerDb.ts
@@ -3,12 +3,16 @@ import mongoose,{Schema,Document} from "mongoose";
 interface Icustomer extends Document{
     name:string,
     email:string,
-    password:string
+    password:string,
+    ppic:string,//personal picture
+    profilepic:string
 }
 const newschemaforcust= new Schema<Icustomer>({
     name:{type:String,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
+    ppic:{type:String},
+    profilepic:{type:String},
 })
 const CustDb= mongoose.model<Icustomer>('Customer',newschemaforcust)
 export default CustDb;
